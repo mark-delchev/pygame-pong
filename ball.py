c25a01823cffcfabdfa7cab46c2e8accf49f4e30
mark-delchev <markdelchev@gmail.com>
2023-06-01 13:36:17 +0300

    fixed multiple game starts

--- ball.py
+++ ball.py
@@ -1,30 +1,21 @@
 import pygame
 from paddle import *
 
-# Initialize Pygame
-pygame.init()
-
-# Set up the game window
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Pong Game")
-
-# Define the Ball class
 class Ball:
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen_height):
         self.x = x
         self.y = y
         self.radius = 5
         self.speed_x = 3
         self.speed_y = 3
+        self.screen_height = screen_height
 
     def update(self):
         self.x += self.speed_x
         self.y += self.speed_y
 
         # Ball collision with the top or bottom of the screen
-        if self.y - self.radius < 0 or self.y + self.radius > screen_height:
+        if self.y - self.radius < 0 or self.y + self.radius > self.screen_height:
             self.speed_y *= -1  # Reverse the y-speed to bounce off the top or bottom
 
     def check_collision(self, paddle):
@@ -43,15 +34,10 @@ class Ball:
             # Reverse the x-speed when colliding with a paddle
             self.speed_x *= -1
 
-    def reset(self):
+    def reset(self, screen_width):
         self.x = screen_width // 2
-        self.y = screen_height // 2
+        self.y = self.screen_height // 2
         self.speed_x *= -1  # Reverse the x-speed for a new direction
 
-    def render(self):
+    def render(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius)
-
-# Create the paddles and the ball
-paddle_a = Paddle(20, screen_height // 2 - 30)
-paddle_b = Paddle(screen_width - 30, screen_height // 2 - 30)
-ball = Ball(screen_width // 2, screen_height // 2)
--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import sys
 from paddle import *
 from ball import *
 
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong Game")
 
 
 # Create the paddles and the ball
 paddle_a = Paddle(20, screen_height // 2 - 30)
 paddle_b = Paddle(screen_width - 30, screen_height // 2 - 30)
-ball = Ball(screen_width // 2, screen_height // 2)
+ball = Ball(screen_width // 2, screen_height // 2, screen_height)
 
 # Main game loop
 def game_loop():
@@ -26,29 +34,29 @@ def game_loop():
 
         # Player A controls
         if keys[pygame.K_w]:
-            paddle_a.update(-1)  # Move paddle A up
+            paddle_a.update(-1, screen_height)  # Move paddle A up
         elif keys[pygame.K_s]:
-            paddle_a.update(1)  # Move paddle A down
+            paddle_a.update(1, screen_height)  # Move paddle A down
 
         # Player B controls
         if keys[pygame.K_UP]:
-            paddle_b.update(-1)  # Move paddle B up
+            paddle_b.update(-1, screen_height)  # Move paddle B up
         elif keys[pygame.K_DOWN]:
-            paddle_b.update(1)  # Move paddle B down
+            paddle_b.update(1, screen_height)  # Move paddle B down
 
 
 
         # Render graphics
         screen.fill((0, 0, 0))  # Clear the screen with a black color
-        paddle_a.render()
-        paddle_b.render()
+        paddle_a.render(screen)
+        paddle_b.render(screen)
         ball.update()
         
-        ball.render()
+        ball.render(screen)
         ball.check_collision(paddle_a)
         ball.check_collision(paddle_b)
         if ball.x - ball.radius > screen_width or ball.x + ball.radius < 0:
-            ball.reset()
+            ball.reset(screen_width)
         # Update the game display
         pygame.display.update()
         clock.tick(60)  # Limit the frame rate to 60 FPS
